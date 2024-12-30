065fd8bb9046723b32c9a6b8f10c7b0de11c3138
Sebastian Russo <sebalexrusso@gmail.com>
2024-12-29 21:20:21 -0600

    (fix) better home page experience for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import Image from 'next/image'
 export default function AboutPage() {
   return (
     <div className="container mx-auto max-w-2xl pt-6 pb-6">
-      <div className="flex gap-8">
+      <div className="flex flex-col md:flex-row gap-8 px-4 md:px-0">
         {/* Profile Picture */}
-        <div className="relative w-[267px] h-[267px] flex-shrink-0">
+        <div className="relative w-[200px] h-[200px] md:w-[267px] md:h-[267px] flex-shrink-0 mx-auto md:mx-0">
           <Image
             src="/images/seb-polaroid.jpeg"
             alt="Sebastian Russo"
@@ -19,7 +19,7 @@ export default function AboutPage() {
         </div>
 
         {/* Content Section */}
-        <div className="flex flex-col justify-start">
+        <div className="flex flex-col justify-start text-center md:text-left">
           <h1 className="text-title tracking-tight mb-6">Sebastian Russo</h1>
           
           <p className="text-base leading-relaxed mb-6">
@@ -30,7 +30,7 @@ export default function AboutPage() {
             This website is a space for my thoughts. You can get in contact by visiting me on socials. 
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex gap-4 justify-center md:justify-start">
             <Link href="https://www.linkedin.com/in/sebasrusso/" className="text-muted-foreground hover:text-foreground">
               <FaLinkedin className="h-6 w-6" />
             </Link>
